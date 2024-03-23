fd3efc154e306e83fe0156a00cf7ba99038a448f
Sergey Yermochenko <yermochenko@gmail.com>
2024-03-23 13:12:02 +0300

    Игра «Black Jack»

--- project-black-jack/src/Game.java
+++ project-black-jack/src/Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
@@ -9,44 +10,60 @@ public class Game {
 
 	public Game(Deck deck, List<Player> players) {
 		this.deck = deck;
+		deck.shuffle();
 		this.players.addAll(players);
+		Collections.shuffle(this.players);
 	}
 
 	public void play(int rounds) {
 		try {
 			for(int round = 1; round <= rounds; round++) {
 				System.out.println("РАУНД #" + round + " НАЧАТ");
-				System.out.println("Сбор ставок игроков...");
+				for(int i = 0; i < players.size(); i++) {
+					System.out.println("    Игрок " + players.get(i).getName() + ": " + players.get(i).getMoney());
+				}
+				System.out.println("    Сбор ставок игроков...");
 				for(int i = 0; i < players.size();) {
 					Player player = players.get(i);
 					Integer bet = player.makeBet();
-					System.out.println("Игрок " + player.getName() + " сделал ставку " + bet);
+					System.out.print("        Игрок " + player.getName() + " сделал ставку " + bet + ". ");
 					try {
 						player.subMoney(bet);
 						bets.add(bet);
 						System.out.println("Ставка принята");
 						i++;
 					} catch(NegativeMoneyException | NotEnoughMoneyException e) {
-						System.out.println("Ставка не принята. Ошибочная сумма: " + e.getMessage());
+						System.out.println("Ставка не принята.\nОшибочная сумма: " + e.getMessage());
 						System.out.println("Игрок " + player.getName() + " дисквалифицирован");
 						players.remove(i);
 					}
 				}
-				System.out.println("Игра:");
+				System.out.println("    Игра...");
 				// сдача карт игрокам
+				System.out.println("        Сдача карт:");
 				for(int i = 0; i < players.size(); i++) {
 					players.get(i).getHand().add(deck.take());
 					players.get(i).getHand().add(deck.take());
+					System.out.println("            Карты игрока " + players.get(i).getName() + ": " + players.get(i).getHand());
 				}
 				// сдача карты крупье
 				croupierHand.add(deck.take());
+				System.out.println("            Карты крупье: " + croupierHand);
 				// игра игроков
+				System.out.println("    Игра:");
 				for(int i = 0; i < players.size(); i++) {
+					System.out.println("        Игрок " + players.get(i).getName());
+					System.out.println("            карты: " + players.get(i).getHand());
 					while(true) {
 						int points = PointsCalculator.calc(players.get(i).getHand());
+						System.out.println("            " + points + " очков");
 						if(points >= 21) break;
-						if(!players.get(i).isNeedMoreCard()) break;
+						if(!players.get(i).isNeedMoreCard()) {
+							System.out.println("            игрок отказался от карты");
+							break;
+						}
 						players.get(i).getHand().add(deck.take());
+						System.out.println("            карты: " + players.get(i).getHand());
 					}
 //					while(PointsCalculator.calc(players.get(i).getHand()) < 21 && players.get(i).isNeedMoreCard()) {
 //						players.get(i).getHand().add(deck.take());
@@ -54,28 +71,48 @@ public class Game {
 				}
 				// игра крупье
 				int croupierPoints;
+				System.out.println("        Крупье");
+				System.out.println("            карты: " + croupierHand);
 				while((croupierPoints = PointsCalculator.calc(croupierHand)) < 17) {
+					System.out.println("            " + croupierPoints + " очков");
 					croupierHand.add(deck.take());
+					System.out.println("            карты: " + croupierHand);
 				}
+				System.out.println("            " + croupierPoints + " очков");
 				// подсчёт выигрыша
 				for(int i = 0; i < players.size(); i++) {
+					System.out.print("        Игрок " + players.get(i).getName() + " ");
 					int points = PointsCalculator.calc(players.get(i).getHand());
 					switch(ResultChecker.check(points, croupierPoints)) {
 						case WIN:
 							try {
 								players.get(i).addMoney(bets.get(i) * 2);
+								System.out.println("выиграл.");
 							} catch(NegativeMoneyException e) {}
 							break;
 						case DRAW:
 							try {
 								players.get(i).addMoney(bets.get(i));
+								System.out.println("сыграл вничью.");
 							} catch(NegativeMoneyException e) {}
 							break;
 						case LOSS:
+							System.out.println("проиграл.");
 					}
 				}
+				for(int i = 0; i < players.size(); i++) {
+					System.out.println("    Игрок " + players.get(i).getName() + ": " + players.get(i).getMoney());
+					players.get(i).getHand().clear();
+				}
+				croupierHand.clear();
+				bets.clear();
 				System.out.println("РАУНДА #" + round + " ОКОНЧЕН");
 				System.out.println("*****************************");
+			}
+			System.out.println("СПИСОК ПОБЕДИТЕЛЕЙ");
+			Collections.sort(players, (player1, player2) -> Double.compare(player2.getMoney(), player1.getMoney()));
+			for(int i = 0; i < players.size() && i < 3; i++) {
+				System.out.println("    " + (i + 1) + "-е место: игрок " + players.get(i).getName() + ", " + players.get(i).getMoney());
 			}
 		} catch(NotEnoughCardsException e) {
 			System.out.println("ОШИБКА: Для продолжения игры не хватает карт в колоде");
--- project-black-jack/src/Main.java
+++ project-black-jack/src/Main.java
@@ -1,36 +1,14 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws NegativeMoneyException {
-		Deck deck = new Deck(5);
-		deck.shuffle();
-		Game game = new Game(deck, List.of(
+		Game game = new Game(new Deck(6), List.of(
 			new Player("Коля", 1000),
 			new Player("Вася", 1000),
-			new Player("Петя", 1000)
+			new Player("Петя", 1000),
+			new Player("Ваня", 1000),
+			new Player("Саша", 1000)
 		));
 		game.play(3);
-//		Scanner keyboard = new Scanner(System.in);
-//		while(!deck.isEmpty()) {
-//			System.out.println("Введите количество карт: ");
-//			int n = keyboard.nextInt();
-//			List<Card> cards = new ArrayList<>();
-//			for(int i = 0; i < n; i++) {
-//				Card card = deck.take();
-//				if(card == null) {
-//					System.out.println("Недостаточно карт в колоде");
-//					keyboard.close();
-//					return;
-//				}
-//				cards.add(card);
-//			}
-//			System.out.println(cards);
-//			System.out.println("Всего очков: " + PointsCalculator.calc(cards));
-//		}
-//		keyboard.close();
-//		Player player = new Player("John", 1000);
-//		player.addMoney(100);
 	}
 }
